d89f29abdf9fa3b19a528d27d53210e635585cb4
DanevskiM <danevskimetodija5@gmail.com>
2024-04-19 00:11:29 +0200

    Create struct_Item & struct_Shopping cart.c

---
+++ OOP/Laboratory Exercises/Lab1/struct_Item & struct_Shopping cart.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+typedef struct Item Item
+{
+    char name[100];
+    int price;
+}
+
+typedef struct ShoppingCart
+{
+    int id_cart;
+    int items;
+}
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++)
+    {
+
+    }
+}
