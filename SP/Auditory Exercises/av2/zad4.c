f468cc9f70a12564bf42ca680bbf6f08b95da8a3
DanevskiM <danevskimetodija5@gmail.com>
2024-04-18 23:56:38 +0200

    Create zad4.c

---
+++ SP/Auditory Exercises/av2/zad4.c
@@ -0,0 +1,17 @@
+/*
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    y = scanf("%d", &x);
+    printf("y = %d\n", y);
+    return 0;
+}*/
+#include <stdio.h>
+int main()
+{
+    int x,y,z;
+    z = scanf("%d%d", &x, &y);
+    printf("z = %d\n", z);
+    return 0;
+}
